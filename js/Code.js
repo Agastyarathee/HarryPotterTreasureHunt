1a64a0828d95daa4b0136ac759ca298f336adb47
Agastyarathee <67199509+Agastyarathee@users.noreply.github.com>
2021-05-05 09:56:40 +0530

    1

---
+++ js/Code.js
@@ -0,0 +1,17 @@
+const accessCode1 = "GRYFFINDOR";
+const accessCode2 = "IAMLORDVOLDEMORT";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("I Y R O F D N G R F", 100,50)
+    fill("lightblue")
+    text("Hint: it is a lion head  !!", 100,70)
+// add code for displaying rest of the names and the hints.
+text("T O M M A R V O L O R I D D L E", 700,150)
+fill("lightblue")
+text("Hint: Harry Potter Fan must know this,are'nt you the one !!", 650,170)
+
+}
